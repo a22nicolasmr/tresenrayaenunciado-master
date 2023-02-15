6f42e9bb1d2b220a25072696b8b96b06410db472
Nicolás <a22nicolasmr@iessanclemente.net>
2023-02-15 10:33:24 +0100

    falta utlimo metodo

--- tresenrayaenunciado-master/src/InterfaceConsola.java
+++ tresenrayaenunciado-master/src/InterfaceConsola.java
@@ -3,87 +3,60 @@ import motor3R.TresEnRaya;
 import java.util.random.*;
 
 public class InterfaceConsola extends TresEnRaya {
-    private char simbolo;
+    
     private char simboloO;
     private int casillaX;
     private int casillaY;
     // private char casillaM;
-    Scanner sc = new Scanner(System.in);
     TresEnRaya a = new TresEnRaya();
 
-    public void PreguntarCosas() {
+    public static char PreguntarCosas() {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Jugador ,escribe el símbolo con el que quieres jugar X o O.");
-        simbolo = sc.next().charAt(0);
+        char simbolo = sc.next().charAt(0);       
+        return simbolo;
+    }
+
+    public static void imprimirTablero(TresEnRaya b, char simbolo) {
+        System.out.println("El tablero sera el siguiente");
+        for (int i = 0; i < b.getTablero().length; i++) {
+            for (int j = 0; j < b.getTablero()[0].length; j++) {
+                System.out.print(" " + b.getTablero()[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("Jugador , donde deseas poner tu " + simbolo);
+    }
+    public static void main(String[] args) {
+        char simbolo;
+        char simboloO;
+        Scanner sc = new Scanner(System.in);
+        InterfaceConsola a1 = new InterfaceConsola();
+        TresEnRaya b = new TresEnRaya();
+        simbolo = PreguntarCosas();
         if (simbolo == 'O') {
             simboloO = 'X';
         } else {
             simboloO = 'O';
         }
-        System.out.println("Vale, jugaras con "+ simbolo+" contra "+simboloO);
-    }
-
-    public void imprimirTablero() {
-        System.out.println("El tablero sera el siguiente");
-        for (int i = 0; i < a.getTablero().length; i++) {
-            for (int j = 0; j < a.getTablero()[0].length; j++) {
-                System.out.print(" "+a.getTablero()[i][j]);
-            }
-            System.out.println("");
-        }
-    }
-
-    public void MeterValores() {
-        System.out.println("Jugador , donde deseas poner tu " + simbolo);
-        int casilla=sc.nextInt();
-        if(casilla==1){
-            a.getTablero()[0][0]=simbolo;
-        }else if (casilla==2) {
-            a.getTablero()[0][1]=simbolo;
-        }else if (casilla==3) {
-            a.getTablero()[0][2]=simbolo;
-        }else if (casilla==4) {
-            a.getTablero()[1][0]=simbolo;
-        }else if (casilla==5) {
-            a.getTablero()[1][1]=simbolo;
-        }else if (casilla==6) {
-            a.getTablero()[1][2]=simbolo;
-        }else if (casilla==7) {
-            a.getTablero()[2][0]=simbolo;
-        }else if (casilla==8) {
-            a.getTablero()[2][1]=simbolo;
-        }else if (casilla==9) {
-            a.getTablero()[2][2]=simbolo;
-        }
-    }
-
-    public void meterValoresMaquina() {
-        int casillaXo=(int)(Math.random()*2-0);
-        int casillaYo=(int)(Math.random()*2-0);
-        while(a.getTablero()[casillaXo][casillaYo]==simbolo){
-            casillaXo=(int)(Math.random()*2-0);
-            casillaYo=(int)(Math.random()*2-0);
-        }
-        System.out.println("El tablero sera el siguiente:");
-        a.getTablero()[casillaXo][casillaYo]=simboloO;
-        for (int i = 0; i < a.getTablero().length; i++) {
-            for (int j = 0; j < a.getTablero()[0].length; j++) {
-                System.out.print(" "+a.getTablero()[i][j]);
-            }
-            System.out.println("");
-        }
-
-    }
-
-    public static void main(String[] args) {
-        InterfaceConsola a1=new InterfaceConsola();
-        a1.PreguntarCosas();
-        a1.imprimirTablero();
-        a1.MeterValores();
-        a1.imprimirTablero();
-        a1.meterValoresMaquina();
-        a1.MeterValores();
-        a1.imprimirTablero();
-        a1.meterValoresMaquina();
+        //primer turno
+        System.out.println("Vale, jugaras con " + simbolo + " contra " + simboloO);
+        imprimirTablero(b, simbolo);
+        int casilla = sc.nextInt();
+        b.MeterValores(casilla,simbolo);
+        b.meterValoresMaquina(simbolo,simboloO);
+        imprimirTablero(b, simbolo);
+        //seegundo turno
+        casilla = sc.nextInt();
+        b.MeterValores(casilla,simbolo);
+        b.meterValoresMaquina(simbolo,simboloO);
+        imprimirTablero(b, simbolo);
+        //Tercer turno
+        casilla = sc.nextInt();
+        b.MeterValores(casilla,simbolo);
+        //////////metodo comprobar ganador
+        b.meterValoresMaquina(simbolo,simboloO);
+        imprimirTablero(b, simbolo);
 
     }
 }
--- tresenrayaenunciado-master/src/motor3R/TresEnRaya.java
+++ tresenrayaenunciado-master/src/motor3R/TresEnRaya.java
@@ -1,24 +1,63 @@
 package motor3R;
+
+import java.util.Scanner;
+
 public class TresEnRaya {
-    public TresEnRaya(){
+    private int casilla;
+    Scanner sc = new Scanner(System.in);
+
+    public TresEnRaya() {
     }
-    private char[][] tablero={
-        {'1','2','3'},
-        {'4','5','6'},
-        {'7','8','9'},
+
+    private char[][] tablero = {
+            { '1', '2', '3' },
+            { '4', '5', '6' },
+            { '7', '8', '9' },
     };
-    public char[][] getTablero(){
+
+    public char[][] getTablero() {
         return tablero;
     }
-    TresEnRaya(char humano,char maquina){
-        for (int i = 0; i < tablero.length; i++) {
-            for (int j = 0; j < tablero[0].length; j++) {
-               
-            }
+
+    public void MeterValores(int casilla,char simbolo) {
+        if (casilla == 1) {
+            this.tablero[0][0] = simbolo;
+        } else if (casilla == 2) {
+            this.tablero[0][1] = simbolo;
+        } else if (casilla == 3) {
+            this.tablero[0][2] = simbolo;
+        } else if (casilla == 4) {
+            this.tablero[1][0] = simbolo;
+        } else if (casilla == 5) {
+            this.tablero[1][1] = simbolo;
+        } else if (casilla == 6) {
+            this.tablero[1][2] = simbolo;
+        } else if (casilla == 7) {
+            this.tablero[2][0] = simbolo;
+        } else if (casilla == 8) {
+            this.tablero[2][1] = simbolo;
+        } else if (casilla == 9) {
+            this.tablero[2][2] = simbolo;
         }
     }
-
-
+    public void meterValoresMaquina(char simbolo,char simboloO) {
+        int casillaXo = (int) (Math.random() * 2 - 0);
+        int casillaYo = (int) (Math.random() * 2 - 0);
+        while (this.tablero[casillaXo][casillaYo] == simbolo || this.tablero[casillaXo][casillaYo] ==simboloO ) {
+            casillaXo = (int) (Math.random() * 2 - 0);
+            casillaYo = (int) (Math.random() * 2 - 0);
+        }
+        this.tablero[casillaXo][casillaYo]=simboloO;
+    }
+    public void comprobarGanador(int casilla,char simbolo,char simboloO){
+       for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[0].length; j++) {
+                if(tablero[i][j]==simbolo){
+                    if(tablero[i][j]==simbolo){
+                        
+                    }
+                }
+            }
+       }
+    }
 }
-
-
